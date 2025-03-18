8289374d414e006d1bae455ce4843cb1c6ff6b80
BobAfwata <bobraphtoneafwata@gmail.com>
2025-03-18 11:18:42 +0900

    introduction to python for machine learning

---
+++ lesson1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3 
+
+print("Bob Afwata") 
+
+print(4)
+print(8 + 10)
+print(100 - 78)
+print(200 / 25)
+print(0.5 * 4)
+
+first_name  = "Bob"
+second_name = "Afwata"
+
+fullName = "Bob Afwata"
+city= "Seoul"
+
+print(first_name + second_name)
+
+print(f"I am  {first_name}  and I live in {city}")
+
+weight = 78.2
+height = 115  # height in cms
+bmi = weight /(height * height)
+print(f"I am {weight} kgs and {height} cms tall with a bmi of {bmi}")
